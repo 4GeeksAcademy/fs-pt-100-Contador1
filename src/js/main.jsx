59109281c1beb6a8acf187b94db7b6da7e382f49
Marta <159387209+martaic@users.noreply.github.com>
2025-04-13 15:07:19 +0000

    contador

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -8,11 +8,33 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+function Contador (props) {
+  return (
+    <div className="Reloj">
+      <div className="alarma"><i class="bi bi-alarm"></i></div>
+      <div className="horas">{props.digitFour % 10}</div>
+      <div className="minutos">{props.digitThree % 10}</div>
+      <div className="segundos">{props.digitTwo % 10}</div>
+      <div className=" milisegundos">{props.digitOne % 10}</div>
+    </div>
+  
+  )
+}
+let counter= 0;
+setInterval(function() {
+  const four = Math.floor (counter/1000)
+  const three= Math.floor (counter/100)
+  const Two= Math.floor (counter/10)
+  const One= Math.floor (counter/1)
+  
+  counter++;
+ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Contador digitOne={One} digitTwo={Two} digitThree={three} digitFour={four} />
   </React.StrictMode>,
-)
+) 
+
+}, 1000);
+
+
+
